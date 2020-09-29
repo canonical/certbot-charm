07d799a495e9e33750d575a856a551fef39f3fa8
Martin Hilton <martin.hilton@canonical.com>
2020-09-29 12:22:06 +0100

    Allow certificate paths to refer to directories
    The paths that certificates are deployed to can now be directories. The
    files will be be copied into those directories with well defined names.
    Also enable running the deploy script as an action.

--- bin/deploy.py
+++ bin/deploy.py
@@ -5,34 +5,49 @@ import configparser
 import os
 import shutil
 import subprocess
+import sys
 
 
-def main():
-    config = configparser.ConfigParser()
-    config.read("/etc/certbot-charm/config.ini")
-    path = os.environ["RENEWED_LINEAGE"]
-    if config["deploy"]["cert-path"]:
-        shutil.copyfile(os.path.join(path, "cert.pem"), config["deploy"]["cert-path"])
-    if config["deploy"]["chain-path"]:
-        shutil.copyfile(os.path.join(path, "chain.pem"), config["deploy"]["chain-path"])
-    if config["deploy"]["combined-path"]:
-        with open(config["deploy"]["combined-path"], "wb") as outf:
-            with open(os.path.join(path, "fullchain.pem"), "rb") as inf:
-                shutil.copyfileobj(inf, outf)
-            with open(os.path.join(path, "privkey.pem"), "rb") as inf:
-                shutil.copyfileobj(inf, outf)
-    if config["deploy"]["fullchain-path"]:
-        shutil.copyfile(
-            os.path.join(path, "fullchain.pem"), config["deploy"]["fullchain-path"]
-        )
-    if config["deploy"]["key-path"]:
-        shutil.copyfile(os.path.join(path, "privkey.pem"), config["deploy"]["key-path"])
-    if config["deploy"]["command"]:
-        try:
-            subprocess.run(config["deploy"]["command"], shell=True)
-        except subprocess.CalledProcessError as err:
-            print("error running deploy command: ", err, file=os.stderr)
+class Deploy:
+    def __init__(self, path, configpath="/etc/certbot-charm/config.ini"):
+        super().__init__()
+        self._path = path
+        self._domain = os.path.basename(path)
+        self._config = configparser.ConfigParser()
+        self._config.read(configpath)
+
+    def run(self):
+        self._copy_file("cert.pem", "cert-path", ".crt")
+        self._copy_file("chain.pem", "chain-path", "_chain.pem")
+
+        dst = self._config["deploy"]["combined-path"]
+        if dst:
+            if os.path.isdir(dst):
+                dst = os.path.join(dst, self._domain + ".pem")
+            with open(dst, "wb") as outf:
+                with open(os.path.join(self._path, "fullchain.pem"), "rb") as inf:
+                    shutil.copyfileobj(inf, outf)
+                with open(os.path.join(self._path, "privkey.pem"), "rb") as inf:
+                    shutil.copyfileobj(inf, outf)
+
+        self._copy_file("fullchain.pem", "fullchain-path", "_fullchain.pem")
+        self._copy_file("privkey.pem", "key-path", ".key")
+
+        cmd = self._config["deploy"]["command"]
+        if cmd:
+            try:
+                subprocess.run(cmd, shell=True)
+            except subprocess.CalledProcessError as err:
+                print("error running deploy command: ", err, file=sys.stderr)
+
+    def _copy_file(self, srcfile, dstkey, suffix):
+        dst = self._config["deploy"].get(dstkey)
+        if not dst:
+            return
+        if os.path.isdir(dst):
+            dst = os.path.join(dst, self._domain + suffix)
+        shutil.copyfile(os.path.join(self._path, srcfile), dst)
 
 
 if __name__ == "__main__":
-    main()
+    Deploy(os.environ["RENEWED_LINEAGE"]).run()
